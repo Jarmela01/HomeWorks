c9c230a1b8f145fa8dcc9aecd3fdb3283ffc9fa5
Your Name <you@example.com>
2023-02-25 20:18:23 +0400

    First Homework C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Sergi Jarmelishvili");
+            Console.WriteLine("Please Enter Words");
+            Console.WriteLine(Console.ReadLine());
+
+        }
+    }
+}
